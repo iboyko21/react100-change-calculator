7e0a76e576b39ad230fbdd48c74402522e50548f
Igor Boyko <iboyko@gmail.com>
2022-06-21 16:55:15 -0700

    all tests pass

--- src/app.jsx
+++ src/app.jsx
@@ -4,13 +4,22 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      aDue: "",
-      aReceived: "",
-      change: ""
+      amountDue: "",
+      amountReceived: "",
+      changeDue: "",
+      twenties: "",
+      tens: "",
+      fives: "",
+      ones: "",
+      quarters: "",
+      dimes: "",
+      nickels: "",
+      pennies: ""
     };
     this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
   }
-
+   
   handleOnChange(event) {
     this.setState({
         [event.target.id]: event.target.value
@@ -18,7 +27,34 @@ class App extends Component {
 }
 
   handleOnClick(){
+    let returnValue = (this.state.amountReceived - this.state.amountDue) * 100;    
+    let change = {};
+    const coinTypes = ["twenty", "ten", "five", "dollar", "quarter", "dime", "nickle", "penny"];
+    const coinValues = [2000, 1000, 500, 100, 25, 10, 5, 1];
+
+    let amount;
+    for (let i = 0; i < coinValues.length; i++){
+        amount = Math.floor(returnValue/coinValues[i]);
+        if (amount > 0) {
+            change[coinTypes[i]] = amount;
+            returnValue = returnValue % coinValues[i];
+        } else if (amount == 0){
+          change[coinTypes[i]] = 0;
+        }
+    }
+    // console.log(change);
     
+    this.setState({
+      changeDue: (this.state.amountReceived - this.state.amountDue).toFixed(2),
+      twenties: change.twenty,
+      tens: change.ten,
+      fives: change.five,
+      ones: change.dollar,
+      quarters: change.quarter,
+      dimes: change.dime,
+      nickels: change.nickle,
+      pennies: change.penny
+    });
   }
 
   render() {
@@ -33,18 +69,18 @@ class App extends Component {
               <div className="card-header">Enter Information</div>
               <div className="card-body">
                 <div className="form-group">
-                  <label htmlFor="total" className="control-label">How much is due?</label>
-                  <input type="number" step="0.01" id="aDue" name="aDue" className="form-control" value={this.state.aDue} onChange={this.handleOnChange}></input>
+                  <label htmlFor="amountDue" className="control-label"><b>How much is due?</b></label>
+                  <input type="number" step="0.01" id="amountDue" name="amountDue" className="form-control" value={this.state.amountDue} onChange={this.handleOnChange}></input>
                 </div>
 
                 <div className="form-group">
-                  <label htmlFor="received" className="control-label">How much was received?</label>
-                  <input type="number" step="0.01" id="aReceived" name="aReceived" className="form-control" value={this.state.aReceived} onChange={this.handleOnChange}></input>
+                  <label htmlFor="amountReceived" className="control-label"><b>How much was received?</b></label>
+                  <input type="number" step="0.01" id="amountReceived" name="amountReceived" className="form-control" value={this.state.amountReceived} onChange={this.handleOnChange}></input>
                 </div>
               </div>
 
               <div className="card-footer">
-                <button type="button" className="btn btn-primary" onClick={this.handleOnClick}>Calculate</button>
+                <button type="button" className="btn btn-primary btn-block" onClick={this.handleOnClick}>Calculate</button>
               </div>
               
             </div>
@@ -53,44 +89,44 @@ class App extends Component {
           <div className="col-sm-8">
             <div className="card">
               <div className="card-body">
-              <p className="bg-success p-2 text-center rounded">Total Change due is</p>
+              <div className="alert alert-success text-center">The total change due is ${this.state.changeDue}</div>
 
               <div className="container">
-                <div className="row text-center">
-                  <div className="col-sm border rounded bg-light mr-3">
-                    <p>Twenties</p>
-                    <p>0</p>
-                  </div>
-                  <div className="col-sm border rounded bg-light mr-3">
-                  <p>Tens</p>
-                    <p>0</p>
-                  </div>
-                  <div className="col-sm border rounded bg-light mr-3">
-                  <p>Fives</p>
-                    <p>0</p>
+                <div className="row text-center g-2">
+                  <div className="col-sm border rounded bg-light">
+                    <h3>Twenties</h3>
+	                  <p className="change">{this.state.twenties}</p>
                   </div>
                   <div className="col-sm border rounded bg-light">
-                  <p>Ones</p>
-                    <p>0</p>
+                    <h3>Tens</h3>
+	                  <p className="change">{this.state.tens}</p>
+                  </div>
+                  <div className="col-sm border rounded bg-light">
+                    <h3>Fives</h3>
+	                  <p className="change">{this.state.fives}</p>
+                  </div>
+                  <div className="col-sm border rounded bg-light">
+                    <h3>Ones</h3>
+	                  <p className="change">{this.state.ones}</p>
                   </div>
                 </div>
 
-                <div className="row text-center">
-                  <div className="col-sm border rounded bg-light mr-3 mt-3">
-                    <p>Quarters</p>
-                    <p>0</p>
+                <div className="row g-1 text-center">
+                  <div className="col-sm border rounded bg-light">
+                    <h3>Quarters</h3>
+	                  <p className="change">{this.state.quarters}</p>
                   </div>
-                  <div className="col-sm border rounded bg-light mr-3 mt-3">
-                    <p>Dimes</p>
-                    <p>0</p>
+                  <div className="col-sm border rounded bg-light">
+                    <h3>Dimes</h3>
+	                  <p className="change">{this.state.dimes}</p>
                   </div>
-                  <div className="col-sm border rounded bg-light mr-3 mt-3">
-                    <p>Nickels</p>
-                    <p>0</p>
+                  <div className="col-sm border rounded bg-light">
+                    <h3>Nickels</h3>
+	                  <p className="change">{this.state.nickels}</p>
                   </div>
-                  <div className="col-sm border rounded bg-light mt-3">
-                    <p>Pennies</p>
-                    <p>0</p>
+                  <div className="col-sm border rounded bg-light">
+                    <h3>Pennies</h3>
+	                  <p className="change">{this.state.pennies}</p>
                   </div>
                 </div>
               </div>
